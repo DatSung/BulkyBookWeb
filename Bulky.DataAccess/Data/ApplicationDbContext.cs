4106ca37b95a1bbc20d3769b726da7e700d0b83a
DatSung <datsung.dev@gmail.com>
2024-01-22 11:22:33 +0700

    Update ProductsController
    Actions CRUD and Views CRUD

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -3,14 +3,102 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.DataAccess.Data
 {
-	public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Action", CategoryDisplayOrder = 1 },
+                new Category { CategoryId = 2, CategoryName = "SciFi", CategoryDisplayOrder = 2 },
+                new Category { CategoryId = 3, CategoryName = "History", CategoryDisplayOrder = 3 }
+                );
+
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProductId = 1,
+                    ProductTitle = "Fortune of Time",
+                    Author = "Billy Spark",
+                    ProductDescription = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "SWD9999001",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80
+                },
+                new Product
+                {
+                    ProductId = 2,
+                    ProductTitle = "Dark Skies",
+                    Author = "Nancy Hoover",
+                    ProductDescription = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "CAW777777701",
+                    ListPrice = 40,
+                    Price = 30,
+                    Price50 = 25,
+                    Price100 = 20
+                },
+                new Product
+                {
+                    ProductId = 3,
+                    ProductTitle = "Vanish in the Sunset",
+                    Author = "Julian Button",
+                    ProductDescription = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "RITO5555501",
+                    ListPrice = 55,
+                    Price = 50,
+                    Price50 = 40,
+                    Price100 = 35
+                },
+                new Product
+                {
+                    ProductId = 4,
+                    ProductTitle = "Cotton Candy",
+                    Author = "Abby Muscles",
+                    ProductDescription = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "WS3333333301",
+                    ListPrice = 70,
+                    Price = 65,
+                    Price50 = 60,
+                    Price100 = 55
+                },
+                new Product
+                {
+                    ProductId = 5,
+                    ProductTitle = "Rock in the Ocean",
+                    Author = "Ron Parker",
+                    ProductDescription = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "SOTJ1111111101",
+                    ListPrice = 30,
+                    Price = 27,
+                    Price50 = 25,
+                    Price100 = 20
+                },
+                new Product
+                {
+                    ProductId = 6,
+                    ProductTitle = "Leaves and Wonders",
+                    Author = "Laura Phantom",
+                    ProductDescription = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "FOT000000001",
+                    ListPrice = 25,
+                    Price = 23,
+                    Price50 = 22,
+                    Price100 = 20
+                }
+
+                );
+        }
 
     }
 }
---
+++ BulkyWeb/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,117 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductsController(IUnitOfWork unitOfWork)
+
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> objProductsList = _unitOfWork.ProductRepository.GetAll().ToList();
+            return View(objProductsList);
+        }
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Product product)
+		{
+
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.ProductRepository.Add(product);
+				_unitOfWork.Save();
+				TempData["Success"] = "Product created successfully";
+				return RedirectToAction("Index", "Products");
+			}
+			return View();
+		}
+
+
+		public IActionResult Edit(int? ProductId)
+		{
+			if (ProductId == null || ProductId == 0)
+			{
+				return NotFound();
+			}
+
+			Product? product = _unitOfWork.ProductRepository.Get(p => p.ProductId == ProductId);
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			return View(product);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Product product)
+		{
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.ProductRepository.Update(product);
+				_unitOfWork.Save();
+				TempData["Success"] = "Product updated successfully";
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+
+
+		public IActionResult Delete(int? ProductId)
+		{
+
+			if (ProductId == null || ProductId == 0)
+			{
+				return NotFound();
+			}
+
+			Product? product = _unitOfWork.ProductRepository.Get(p => p.ProductId == ProductId);
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			return View(product);
+		}
+
+		[HttpPost]
+		[ActionName("Delete")]
+		public IActionResult DeletePost(int? ProductId)
+		{
+			if (ProductId == null || ProductId == 0)
+			{
+				return NotFound();
+			}
+
+			Product? product = _unitOfWork.ProductRepository.Get(p => p.ProductId == ProductId);
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			_unitOfWork.ProductRepository.Remove(product);
+			_unitOfWork.Save();
+			TempData["Success"] = "Product deleted successfully";
+
+			return RedirectToAction("Index", "Products");
+		}
+	}
+}
